40cabc22087caa6a998aa0507afe776a5d04c1e6
Hiroyuki Saito <psyhiro@yahoo.co.jp>
2018-06-24 01:29:09 +0900

    Wrote development strategy comment

--- js/app.js
+++ js/app.js
@@ -25,14 +25,47 @@ function shuffle(array) {
     return array;
 }
 
+/*
+ Set up the event listener for a card. If a card is clicked:
+*/
+
 
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+ Display the card's symbol (put this functionality in another function that you call from this one)
+*/
+
+
+/*
+ Add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+*/
+
+/*
+Start by building a very simple grid of cards.
+Don't worry about styling, just get something clickable on the page.
+*/
+
+/*
+Figure out the HTML needed to represent a card. Remember, you have to represent two sides of the card. Are you going to have two separate elements stacked on top of each other?
+*/
+
+/*
+Add the functionality to handle clicks.
+This should reveal the hidden side of each card.
+*/
+
+/*
+Work on the matching logic. How does your game "know" if a player guesses correctly or incorrectly?
+
+ If the list already has another card, check to see if the two cards match
+
+ If the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+
+ If the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+
+ Increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+*/
+
+/*
+Work on the winning condition.
+ If all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+*/
